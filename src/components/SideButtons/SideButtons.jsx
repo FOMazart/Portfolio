469bf90e600e7280db8a74aa2af6ced1c93baa9e
FOMazart <apakistanov071@gmail.com>
2025-03-02 11:39:39 +0500

    Side Button Animation

--- src/components/SideButtons/SideButtons.jsx
+++ src/components/SideButtons/SideButtons.jsx
@@ -1,14 +1,14 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import './SideButtons.css';
   
-
-
 const SideButtons = () => {
+  const [activeSection, setActiveSection] = useState('');
+
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
     if (section) {
       section.scrollIntoView({ behavior: 'smooth' });
+      setActiveSection(sectionId);
     }
   };
 
@@ -17,23 +17,41 @@ const SideButtons = () => {
       <nav>
         <ul>
           <li>
-            <button onClick={() => scrollToSection('home')}> <img src="../../assets/icons/home_bar.svg" className='home-bar'/> </button>
+            <button 
+              onClick={() => scrollToSection('home')} 
+              className={activeSection === 'home' ? 'active' : ''}
+            >
+              <img src="../../assets/icons/home_icon.svg" className={`home-bar ${activeSection === 'home' ? 'active-img' : ''}`}/> 
+            </button>
           </li>
           <li>
-            <button onClick={() => scrollToSection('about')}><img src="../../assets/icons/person.svg" className='person-bar'/></button>
+            <button 
+              onClick={() => scrollToSection('about')} 
+              className={activeSection === 'about' ? 'active' : ''}
+            >
+              <img src="../../assets/icons/about_icon.svg" className={`person-bar ${activeSection === 'about' ? 'active-img' : ''}`}/>
+            </button>
           </li>
           <li>
-            <button onClick={() => scrollToSection('work')}><img src="../../assets/icons/work_bar.svg" className='work-bar'/></button>
+            <button 
+              onClick={() => scrollToSection('work')} 
+              className={activeSection === 'work' ? 'active' : ''}
+            >
+              <img src="../../assets/icons/work_icon.svg" className={`work-bar ${activeSection === 'work' ? 'active-img' : ''}`}/>
+            </button>
           </li>
           <li>
-            <button onClick={() => scrollToSection('contact')}><img src="../../assets/icons/Phone_bar.svg" className='Phone-bar'/></button>
+            <button 
+              onClick={() => scrollToSection('contact')} 
+              className={activeSection === 'contact' ? 'active' : ''}
+            >
+              <img src="../../assets/icons/contact_icon.svg" className={`Phone-bar ${activeSection === 'contact' ? 'active-img' : ''}`}/>
+            </button>
           </li>
         </ul>
       </nav>
     </div>
-    
   );
-
 };
 
 export default SideButtons;
