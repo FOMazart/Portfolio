73b48e0adafc3b973f33a8245436e66711eeb5fc
FOMazart <apakistanov071@gmail.com>
2025-03-02 12:18:10 +0500

    revert wave animation

--- src/components/SideButtons/SideButtons.jsx
+++ src/components/SideButtons/SideButtons.jsx
@@ -1,58 +1,54 @@
-import React, { useState, useRef } from 'react';
-import { WaveEffect } from '../components/WaveEffect';
+import React, { useState } from 'react';
 import './SideButtons.css';
   
 const SideButtons = () => {
   const [activeSection, setActiveSection] = useState('');
-  const [activeElement, setActiveElement] = useState(null);
 
-  const scrollToSection = (sectionId, event) => {
+  const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
     if (section) {
       section.scrollIntoView({ behavior: 'smooth' });
       setActiveSection(sectionId);
-      setActiveElement(event.currentTarget);
     }
   };
 
   return (
     <div className="side-buttons">
-      <nav style={{ position: 'relative' }}>
+      <nav>
         <ul>
           <li>
-            <button onClick={(e) => scrollToSection('home', e)}>
-              <img 
-                src="../../assets/icons/home_icon.svg" 
-                className={activeSection === 'home' ? 'active-img' : ''} 
-              /> 
+            <button 
+              onClick={() => scrollToSection('home')} 
+              className={activeSection === 'home' ? 'active' : ''}
+            >
+              <img src="../../assets/icons/home_icon.svg" className={`home-bar ${activeSection === 'home' ? 'active-img' : ''}`}/> 
             </button>
           </li>
           <li>
-            <button onClick={(e) => scrollToSection('about', e)}>
-              <img 
-                src="../../assets/icons/about_icon.svg" 
-                className={activeSection === 'about' ? 'active-img' : ''} 
-              />
+            <button 
+              onClick={() => scrollToSection('about')} 
+              className={activeSection === 'about' ? 'active' : ''}
+            >
+              <img src="../../assets/icons/about_icon.svg" className={`person-bar ${activeSection === 'about' ? 'active-img' : ''}`}/>
             </button>
           </li>
           <li>
-            <button onClick={(e) => scrollToSection('work', e)}>
-              <img 
-                src="../../assets/icons/work_icon.svg" 
-                className={activeSection === 'work' ? 'active-img' : ''} 
-              />
+            <button 
+              onClick={() => scrollToSection('work')} 
+              className={activeSection === 'work' ? 'active' : ''}
+            >
+              <img src="../../assets/icons/work_icon.svg" className={`work-bar ${activeSection === 'work' ? 'active-img' : ''}`}/>
             </button>
           </li>
           <li>
-            <button onClick={(e) => scrollToSection('contact', e)}>
-              <img 
-                src="../../assets/icons/contact_icon.svg" 
-                className={activeSection === 'contact' ? 'active-img' : ''} 
-              />
+            <button 
+              onClick={() => scrollToSection('contact')} 
+              className={activeSection === 'contact' ? 'active' : ''}
+            >
+              <img src="../../assets/icons/contact_icon.svg" className={`Phone-bar ${activeSection === 'contact' ? 'active-img' : ''}`}/>
             </button>
           </li>
         </ul>
-        <WaveEffect activeElement={activeElement} />
       </nav>
     </div>
   );
