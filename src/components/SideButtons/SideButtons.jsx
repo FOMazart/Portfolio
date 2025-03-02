b61a7a45c073a93753719d8e8af9927212006c89
aakhetov <aakhetov@rnm.dev>
2025-03-02 11:49:17 +0500

    side buttons animation fix

--- src/components/SideButtons/SideButtons.jsx
+++ src/components/SideButtons/SideButtons.jsx
@@ -17,35 +17,35 @@ const SideButtons = () => {
       <nav>
         <ul>
           <li>
-            <button 
-              onClick={() => scrollToSection('home')} 
-              className={activeSection === 'home' ? 'active' : ''}
-            >
-              <img src="../../assets/icons/home_icon.svg" className={`home-bar ${activeSection === 'home' ? 'active-img' : ''}`}/> 
+            <button onClick={() => scrollToSection('home')}>
+              <img 
+                src="../../assets/icons/home_icon.svg" 
+                className={activeSection === 'home' ? 'active-img' : ''} 
+              /> 
             </button>
           </li>
           <li>
-            <button 
-              onClick={() => scrollToSection('about')} 
-              className={activeSection === 'about' ? 'active' : ''}
-            >
-              <img src="../../assets/icons/about_icon.svg" className={`person-bar ${activeSection === 'about' ? 'active-img' : ''}`}/>
+            <button onClick={() => scrollToSection('about')}>
+              <img 
+                src="../../assets/icons/about_icon.svg" 
+                className={activeSection === 'about' ? 'active-img' : ''} 
+              />
             </button>
           </li>
           <li>
-            <button 
-              onClick={() => scrollToSection('work')} 
-              className={activeSection === 'work' ? 'active' : ''}
-            >
-              <img src="../../assets/icons/work_icon.svg" className={`work-bar ${activeSection === 'work' ? 'active-img' : ''}`}/>
+            <button onClick={() => scrollToSection('work')}>
+              <img 
+                src="../../assets/icons/work_icon.svg" 
+                className={activeSection === 'work' ? 'active-img' : ''} 
+              />
             </button>
           </li>
           <li>
-            <button 
-              onClick={() => scrollToSection('contact')} 
-              className={activeSection === 'contact' ? 'active' : ''}
-            >
-              <img src="../../assets/icons/contact_icon.svg" className={`Phone-bar ${activeSection === 'contact' ? 'active-img' : ''}`}/>
+            <button onClick={() => scrollToSection('contact')}>
+              <img 
+                src="../../assets/icons/contact_icon.svg" 
+                className={activeSection === 'contact' ? 'active-img' : ''} 
+              />
             </button>
           </li>
         </ul>
